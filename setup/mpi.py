1994746d9c59ff4fd6d50676ae86091782b47256
Newton Ni <nwtnni@gmail.com>
2023-05-04 13:07:29 -0500

    Measure broadcast for all algorithms

--- setup/mpi.py
+++ setup/mpi.py
@@ -5,6 +5,58 @@ import time
 from fabric import Connection
 from fabric import ThreadingGroup
 
+# ompi_info --param coll tuned --level 5
+BROADCAST = [
+    "ignore",
+    "basic-linear",
+    "chain",
+    "pipeline",
+    "split-binary-tree",
+    "binary-tree",
+    "binomial-tree",
+    "knomial-tree",
+    "scatter-allgather",
+    "scatter-allgather-ring",
+]
+
+def broadcast(nodes, interface, algorithm):
+    nodes.sudo(f"ethtool -C {interface} stats-block-usecs 100000")
+
+    ifstat_out = f"ifstat-broadcast-{BROADCAST[algorithm]}.txt"
+    ifstats = nodes.run(f"~/ifstat -I {interface} -d 120s -i 100ms > {ifstat_out}", asynchronous=True)
+
+    pcm_out = f"pcm-broadcast-{BROADCAST[algorithm]}.txt"
+    pcms = nodes.sudo(f"pcm -nc -csv={pcm_out} -i=1200 0.1", asynchronous=True)
+
+    time.sleep(5.0)
+
+    # https://github.com/JiaweiZhuang/aws-mpi-benchmark/tree/master
+    command = " ".join([
+        "OPAL_PREFIX=/users/nwtnni/openmpi-v5.0.x-install",
+        "mpirun",
+        "-x OPAL_PREFIX",
+        "--map-by ppr:1:node",
+        "--mca btl self,tcp",
+        f"--mca btl_tcp_if_include {interface}",
+        "-H",
+        ",".join([host.split('@')[1] for host in hosts]),
+        "--mca coll_tuned_use_dynamic_rules 1",
+        f"--mca coll_tuned_bcast_algorithm {algorithm}",
+        "broadcast",
+        "$((2**30))",
+    ])
+
+    nodes[0].run(command)
+
+    for ifstat in ifstats.values():
+        ifstat.join()
+    for pcm in pcms.values():
+        pcm.join()
+
+    for index, host in enumerate(hosts):
+        subprocess.run(["scp", f"{host}:~/{ifstat_out}", f"{index}-{ifstat_out}"])
+        subprocess.run(["scp", f"{host}:~/{pcm_out}", f"{index}-{pcm_out}"])
+
 
 if __name__ == "__main__":
     interface = None
@@ -16,37 +68,8 @@ if __name__ == "__main__":
         exit(1)
 
     hosts = [host.strip() for host in sys.stdin.readlines()]
-    nodes = [Connection(host, forward_agent=True) for host in hosts]
+    nodes = ThreadingGroup(*hosts, forward_agent=True)
 
-    ifstats = [node.run(f"~/ifstat -I {interface} -i 20ms -d 15s > ifstat-broadcast.txt", asynchronous=True) for node in nodes]
-    pcms = [node.sudo("timeout 15s pcm-memory -nc -s -csv=pcm-memory-broadcast.txt 0.02", asynchronous=True, warn=True) for node in nodes]
-
-    time.sleep(1.0)
-
-    # https://github.com/JiaweiZhuang/aws-mpi-benchmark/tree/master
-    # for algorithm in range(10):
-    command = " ".join([
-        "OPAL_PREFIX=/users/nwtnni/openmpi-v5.0.x-install",
-        "mpirun",
-        "-x OPAL_PREFIX",
-        "--map-by ppr:1:node",
-        "--mca btl self,tcp",
-        f"--mca btl_tcp_if_include {interface}",
-        "-H",
-        ",".join([host.split('@')[1] for host in hosts]),
-        # "--mca coll_tuned_use_dynamic_rules 1",
-        # f"--mca coll_tuned_bcast_algorithm {algorithm}",
-        "broadcast",
-        "$((2**30))",
-    ])
-
-    nodes[0].run(command)
-
-    for ifstat in ifstats:
-        ifstat.join()
-    for pcm in pcms:
-        pcm.join()
-
-    for index, host in enumerate(hosts):
-        subprocess.run(["scp", f"{host}:~/ifstat-broadcast.txt", f"{index}-ifstat-broadcast.txt"])
-        subprocess.run(["scp", f"{host}:~/pcm-memory-broadcast.txt", f"{index}-pcm-memory-broadcast.txt"])
+    for algorithm in range(len(BROADCAST)):
+        print(f"Running algorithm {BROADCAST[algorithm]}")
+        broadcast(nodes, interface, algorithm)

4d3f36848cb8c2837610e7990f9cd9b874ced1ae
Newton Ni <nwtnni@gmail.com>
2023-05-04 13:55:03 -0500

    Generalize to other benchmarks

--- setup/mpi.py
+++ setup/mpi.py
@@ -2,30 +2,44 @@ import subprocess
 import sys
 import time
 
+import click
 from fabric import Connection
 from fabric import ThreadingGroup
 
 # ompi_info --param coll tuned --level 5
-BROADCAST = [
-    "ignore",
-    "basic-linear",
-    "chain",
-    "pipeline",
-    "split-binary-tree",
-    "binary-tree",
-    "binomial-tree",
-    "knomial-tree",
-    "scatter-allgather",
-    "scatter-allgather-ring",
-]
+ALGORITHMS = {
+    "allreduce": [
+        "ignore",
+        "basic-linear",
+        "nonoverlapping",
+        "recursive-doubling",
+        "ring",
+        "segmented-ring",
+    ],
+    "broadcast": [
+        "ignore",
+        "basic-linear",
+        "chain",
+        "pipeline",
+        "split-binary-tree",
+        "binary-tree",
+        "binomial-tree",
+        "knomial-tree",
+        "scatter-allgather",
+        "scatter-allgather-ring",
+    ],
+}
 
-def broadcast(nodes, interface, algorithm):
+
+def run(hosts, nodes, interface, benchmark, algorithm):
+    print(f"Running {benchmark} {ALGORITHMS[benchmark][algorithm]}...")
+
     nodes.sudo(f"ethtool -C {interface} stats-block-usecs 100000")
 
-    ifstat_out = f"ifstat-broadcast-{BROADCAST[algorithm]}.txt"
+    ifstat_out = f"ifstat-{benchmark}-{ALGORITHMS[benchmark][algorithm]}.txt"
     ifstats = nodes.run(f"~/ifstat -I {interface} -d 120s -i 100ms > {ifstat_out}", asynchronous=True)
 
-    pcm_out = f"pcm-broadcast-{BROADCAST[algorithm]}.txt"
+    pcm_out = f"pcm-{benchmark}-{ALGORITHMS[benchmark][algorithm]}.txt"
     pcms = nodes.sudo(f"pcm -nc -csv={pcm_out} -i=1200 0.1", asynchronous=True)
 
     time.sleep(5.0)
@@ -41,8 +55,8 @@ def broadcast(nodes, interface, algorithm):
         "-H",
         ",".join([host.split('@')[1] for host in hosts]),
         "--mca coll_tuned_use_dynamic_rules 1",
-        f"--mca coll_tuned_bcast_algorithm {algorithm}",
-        "broadcast",
+        f"--mca coll_tuned_{'bcast' if benchmark == 'broadcast' else benchmark}_algorithm {algorithm}",
+        benchmark,
         "$((2**30))",
     ])
 
@@ -63,18 +77,33 @@ def broadcast(nodes, interface, algorithm):
         subprocess.Popen(["mv", f"{host}-{pcm_out}", f"{index}-{pcm_out}"])
 
 
-if __name__ == "__main__":
-    interface = None
-
-    try:
-        interface = sys.argv[1]
-    except IndexError:
-        print("Usage: python mpi.py <NETWORK_INTERFACE>")
-        exit(1)
-
+@click.command()
+@click.option("-i", "--interface", required=True)
+@click.option("-b", "--benchmark")
+@click.option("-a", "--algorithm")
+def main(interface, benchmark, algorithm):
     hosts = [host.strip() for host in sys.stdin.readlines()]
     nodes = ThreadingGroup(*hosts, forward_agent=True)
 
-    for algorithm in range(len(BROADCAST)):
-        print(f"Running algorithm {BROADCAST[algorithm]}")
-        broadcast(nodes, interface, algorithm)
+    assert benchmark in ALGORITHMS.keys()
+
+    if algorithm is not None:
+        assert benchmark is not None
+        try:
+            algorithm = int(algorithm)
+            assert algorithm >= 0
+            assert algorithm < len(ALGORITHMS[benchmark])
+        except ValueError:
+            assert algorithm in ALGORITHMS[benchmark]
+            algorithm = ALGORITHMS[benchmark].index(algorithm)
+
+        run(hosts, nodes, interface, benchmark, algorithm)
+        return
+
+    for benchmark in [benchmark] if benchmark is not None else ALGORITHMS.keys():
+        for algorithm in range(len(ALGORITHMS[benchmark])):
+            run(hosts, nodes, interface, benchmark, algorithm)
+
+
+if __name__ == "__main__":
+    main()

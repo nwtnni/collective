e8b6a075fea2801c054e988f3b8258726d5df05f
Newton Ni <nwtnni@gmail.com>
2023-05-08 14:58:19 -0500

    Fix bugs in iperf script
    Connect to internal network instead of external.

--- experiments/iperf.py
+++ experiments/iperf.py
@@ -9,22 +9,22 @@ from fabric import ThreadingGroup
 @click.option("-u", "--user", required=True)
 def main(user):
     hosts = [host.strip() for host in sys.stdin.readlines()]
-    nodes = ThreadingGroup(host, user=user)
+    nodes = ThreadingGroup(*hosts, user=user)
 
     # Sanity check
     indices = [int(node.run("hostname -s").stdout.strip().lstrip("node-")) for node in nodes]
     assert(indices == sorted(indices))
 
-    servers = node.run("iperf -s", asynchronous=True)
+    servers = nodes.run("iperf -s", asynchronous=True)
     time.sleep(3.0)
 
     for i, node in enumerate(nodes):
-        for j, host in enumerate(hosts):
+        for j in range(len(nodes)):
             if i == j:
                 continue
 
             with open(f"{i}-to-{j}.txt", "w") as file:
-                file.write(node.run(f"iperf -c {host}").stdout)
+                file.write(node.run(f"iperf -c 10.0.1.{j + 1}").stdout)
 
     nodes.run("kill $(ps -A | grep iperf | cut -f3 -d' ')")
     for server in servers.values():

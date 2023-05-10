fb2eeaf0052bc49904634d1f644ff87d0202aed5
Newton Ni <nwtnni@gmail.com>
2023-05-10 17:10:36 -0500

    Intercept MPI_Bcast and MPI_Barrier
    The `MPI_Datatype` and `MPI_Comm` arguments don't look right, and
    the program is segfaulting.

--- collective/src/lib.rs
+++ collective/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(clippy::missing_safety_doc)]
+#![allow(non_camel_case_types)]
 
 use std::ffi;
 
@@ -25,4 +26,45 @@ pub unsafe extern "C" fn MPI_Init_thread(
         .unwrap();
 
     mpi_init_thread(argc, argv, required, provided)
+}
+
+type MPI_Datatype = ffi::c_int;
+type MPI_Comm = ffi::c_int;
+
+#[no_mangle]
+pub unsafe extern "C" fn MPI_Bcast(
+    buffer: *const ffi::c_void,
+    count: ffi::c_int,
+    datatype: MPI_Datatype,
+    root: ffi::c_int,
+    comm: MPI_Comm,
+) -> ffi::c_int {
+    println!(
+        "Called MPI_Bcast with arguments: {buffer:?} {count:?} {datatype:?} {root:?} {comm:?}",
+    );
+
+    let library = libloading::Library::new("libmpi.so").unwrap();
+    let mpi_bcast = library
+        .get::<unsafe extern "C" fn(
+            *const ffi::c_void,
+            ffi::c_int,
+            MPI_Datatype,
+            ffi::c_int,
+            MPI_Comm,
+        ) -> ffi::c_int>(b"MPI_Bcast\0")
+        .unwrap();
+
+    mpi_bcast(buffer, count, datatype, root, comm)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn MPI_Barrier(comm: MPI_Comm) -> ffi::c_int {
+    println!("Called MPI_Barrier with arguments: {comm:?}");
+
+    let library = libloading::Library::new("libmpi.so").unwrap();
+    let mpi_barrier = library
+        .get::<unsafe extern "C" fn(MPI_Comm) -> ffi::c_int>(b"MPI_Barrier\0")
+        .unwrap();
+
+    mpi_barrier(comm)
 }

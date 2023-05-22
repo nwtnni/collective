168e41a99d4b0e3f4bb4f41fff89de1f4d720772
Newton Ni <nwtnni@gmail.com>
2023-05-22 10:00:04 -0500

    Update VM helper scripts
    Set default VMs to eight.

--- experiments/execute.sh
+++ experiments/execute.sh
@@ -2,9 +2,8 @@
 set -euxo pipefail
 
 num_vms="${num_vms:-8}"
-dir="${dir:-$(dirname $1)}"
 
 for ((i = 0; i < $num_vms; i++)); do
   ip="172.16.0.$(expr 2 + $i)"
-  scp -o StrictHostKeyChecking=false $1 root@$ip:"$dir/$(basename $1)"
+  ssh -o StrictHostKeyChecking=false root@$ip $1
 done

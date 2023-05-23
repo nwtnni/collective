fe7e20aebb38328c85e3e2878b5d94f7e3af7255
Newton Ni <nwtnni@gmail.com>
2023-05-23 15:52:13 -0500

    Add metrics for interrupts + multiple buffers

---
+++ experiments/wisc-c220g5/allreduce-cdim/command.sh
@@ -0,0 +1,22 @@
+LD_PRELOAD=./libcollective.so \
+    COLLECTIVE_ALLREDUCE_ALGORITHM=multiple \
+    COLLECTIVE_PCI_PATH=/dev/ivpci0 \
+    COLLECTIVE_PCI_SIZE=$((2**31)) \
+    mpirun \
+    -x LD_PRELOAD \
+    -x COLLECTIVE_ALLREDUCE_ALGORITHM \
+    -x COLLECTIVE_PCI_PATH \
+    -x COLLECTIVE_PCI_SIZE \
+    -H 172.16.0.2,172.16.0.3,172.16.0.4,172.16.0.5,172.16.0.6,172.16.0.7,172.16.0.8,172.16.0.9 \
+    --map-by ppr:1:node \
+    --mca btl self,tcp \
+    --mca btl_tcp_if_include lo,enp0s4 \
+    --output-filename metrics-1m \
+    --allow-run-as-root \
+    /root/collective-bench \
+    -i 1 \
+    -w 100 \
+    -s $((2**20)) \
+    -v \
+    allreduce \
+    -o sum
